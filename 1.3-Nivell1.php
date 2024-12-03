dd7556c961408cc4dbc831022d7e75c891a76086
LenaPradoP <lena@atajo.es>
2024-12-03 11:59:08 +0100

    Ejercicio 2 actualizado

--- 1.3-Nivell1.php
+++ 1.3-Nivell1.php
@@ -11,17 +11,11 @@ for ($i = 0; $i <= count($numeros); $i++) {
 echo "<h1><u> Ejercicio 2 </u></h1>";
     $X = array (10, 20, 30, 40, 50, 60);
 
-/*Mostrar per pantalla la mida de l’array anterior i posteriorment elimina un element de l’array anterior.
-Després d'eliminar l'element, les claus senceres han de ser normalitzades(s’han de reorganitzar els seus índexs).
-Mostra per última vegada la mida de l’array.*/
-
 echo count($X) . "<br>";
-
-echo var_dump($X) . "<br>";
 
 unset($X [4]);
 
-echo var_dump($X) . "<br>";
+$X = array_values($X);
 
 echo count($X) . "<br>";
 
