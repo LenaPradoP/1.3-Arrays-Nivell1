57fb42652d0758ab9a15f66e22284a5aa69b883d
LenaPradoP <lena@atajo.es>
2024-12-03 19:32:44 +0100

    Correción ejercicio 1

--- 1.3-Nivell1.php
+++ 1.3-Nivell1.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 echo "<h1><u> Ejercicio 1 </u></h1>";
     $numeros = array(2, 7, 12, 43, 29);
 
-for ($i = 0; $i <= count($numeros); $i++) {
+for ($i = 0; $i < count($numeros); $i++) {
     echo "$numeros[$i] <br>";
 }
 
